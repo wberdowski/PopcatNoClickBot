5e64c0beb3cbfed45e75703ac7ee3f88365f1ce9
Wojciech Berdowski <10144015+wberdowski@users.noreply.github.com>
2021-04-21 22:44:26 +0200

    Initial commit

---
+++ source.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @name         Popcat no-click bot
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Ban-proof popcat.click bot
+// @author       Wojciech Berdowski
+// @match        https://popcat.click/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    console.clear()
+
+    var event = new KeyboardEvent('keydown', {
+        key: 'g',
+        ctrlKey: true
+    });
+
+    // Start sendStats interval
+    document.dispatchEvent(event);
+    // Total pops
+    var total = 0;
+
+    var iv = setInterval(() => {
+        // Get VUE
+        var vue = document.getElementById('app').__vue__;
+        // Check if user is marked as bot (just for safety measures, very unlikely to happen)
+        if (vue.bot) {
+            console.log("%c You've been barked as a bot. Please clear your cookies.", "background: #a00; color: #fff");
+            clearInterval(iv);
+            return;
+        }
+        // Prevent ban
+        vue.sequential_max_pops = 0;
+        // Detect sendStats function run
+        if (vue.accumulator == 0) {
+            total += 800;
+            console.log(`[${new Date().toLocaleTimeString()}] %c800 pops sent (Total: ${total})`, "color: #0f0");
+            // Open and close cat's mount
+            vue.open = true;
+            setTimeout(() => {
+                vue.open = false;
+            }, 1000);
+        }
+        // Set 800 pops
+        vue.accumulator = 800;
+    }, 1000);
+
+    console.log("%c Bot started. Waiting for the first request being sent. ", "background: #050; color: #0f0");
+})();
